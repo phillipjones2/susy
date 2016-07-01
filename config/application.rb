e324384a02faacdcc0dd4d9d7f57071ed73b2254
Phillip Jones <phillipemersonjones@gmail.com>
2016-06-30 20:31:22 -0500

    susy

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
+require 'susy'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module Susy
   class Application < Rails::Application
